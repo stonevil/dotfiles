3e344955cae71c892d1b36c65d8f87f3ddb047b6
Myroslav Rys <stonevil@gmail.com>
2021-12-28 19:49:44 -0800

    Add change directory

--- dot_shellrc/media.sh
+++ dot_shellrc/media.sh
@@ -2,6 +2,7 @@
 # vim:ft=sh :
 
 ydl() {
+	cd "$HOME"/Downloads || exit 1
 	if youtube-dl -f 'bestvideo[height<=720]+bestaudio[ext=m4a]/best[height<=720]' --recode-video mp4 --audio-format m4a --merge-output-format mp4 "$@"; then
 		terminal-notifier -title "Video Download Finished" -sound default -message "$@"
 	else
@@ -10,6 +11,7 @@ ydl() {
 }
 
 ymdl() {
+	cd "$HOME"/Downloads || exit 1
 	if youtube-dl -x --audio-format m4a --embed-thumbnail "$@"; then
 		terminal-notifier -title "Audio Download Finished" -sound default -message "$@"
 	else
