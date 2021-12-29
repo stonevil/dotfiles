bab18624c323451a2c9ba3b7bdb34a12533865e8
Myroslav Rys <stonevil@gmail.com>
2021-12-28 19:14:51 -0800

    Add notification

--- dot_shellrc/media.sh
+++ dot_shellrc/media.sh
@@ -2,9 +2,17 @@
 # vim:ft=sh :
 
 ydl() {
-	youtube-dl -f 'bestvideo[height<=720]+bestaudio[ext=m4a]/best[height<=720]' --recode-video mp4 --audio-format m4a --merge-output-format mp4 "$@"
+	if youtube-dl -f 'bestvideo[height<=720]+bestaudio[ext=m4a]/best[height<=720]' --recode-video mp4 --audio-format m4a --merge-output-format mp4 "$@"; then
+		terminal-notifier -title "Video Download" -subtitle 'Video download finished' -appIcon /System/Library/PrivateFrameworks/PhotoPrintProduct.framework/Versions/A/Resources/alert-yellow_large.tiff -sound Hero -ignoreDnD -message "$@"
+	else
+		terminal-notifier -title "Video Download" -subtitle 'Video download failed' -appIcon /System/Library/PrivateFrameworks/PhotoPrintProduct.framework/Versions/A/Resources/alert-yellow_large.tiff -sound Hero -ignoreDnD -message "$@"
+	fi
 }
 
 ymdl() {
-	youtube-dl -x --audio-format m4a --embed-thumbnail "$@"
+	if youtube-dl -x --audio-format m4a --embed-thumbnail "$@"; then
+		terminal-notifier -title "Audio Download" -subtitle 'Audio download finished' -appIcon /System/Library/PrivateFrameworks/PhotoPrintProduct.framework/Versions/A/Resources/cloud_download_image.png -sound Hero -ignoreDnD -message "$@"
+	else
+		terminal-notifier -title "Audio Download" -subtitle 'Audio download failed' -appIcon /System/Library/PrivateFrameworks/PhotoPrintProduct.framework/Versions/A/Resources/cloud_download_image.png -sound Hero -ignoreDnD -message "$@"
+	fi
 }
