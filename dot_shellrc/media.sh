23eb0e802004bf7cfe9f046b3d8a055e339535bd
Myroslav Rys <stonevil@gmail.com>
2021-12-28 20:13:24 -0800

    Switch to use name

--- dot_shellrc/media.sh
+++ dot_shellrc/media.sh
@@ -3,18 +3,20 @@
 
 ydl() {
 	cd "$HOME"/Downloads || exit 1
+	video_title=$(youtube-dl --skip-download --get-title --no-warnings "$@" | sed 2d)
 	if youtube-dl -f 'bestvideo[height<=720]+bestaudio[ext=m4a]/best[height<=720]' --recode-video mp4 --audio-format m4a --merge-output-format mp4 "$@"; then
-		terminal-notifier -title "Video Download Finished" -sound default -message "$@"
+		terminal-notifier -title "Video Download Finished" -sound default -message "$video_title"
 	else
-		terminal-notifier -title "Video Download Failed" -sound default -message "$@"
+		terminal-notifier -title "Video Download Failed" -sound default -message "$video_title"
 	fi
 }
 
 ymdl() {
 	cd "$HOME"/Downloads || exit 1
+	video_title=$(youtube-dl --skip-download --get-title --no-warnings "$@" | sed 2d)
 	if youtube-dl -x --audio-format m4a --embed-thumbnail "$@"; then
-		terminal-notifier -title "Audio Download Finished" -sound default -message "$@"
+		terminal-notifier -title "Video Download Finished" -sound default -message "$video_title"
 	else
-		terminal-notifier -title "Audio Download Failed" -sound default -message "$@"
+		terminal-notifier -title "Video Download Failed" -sound default -message "$video_title"
 	fi
 }
