a8b0247b46cc1c2b22655bf2c65aeecf73166a97
Myroslav Rys <stonevil@gmail.com>
2022-09-08 10:17:44 -0700

    Fix yt-dlp

--- dot_shellrc/media.sh
+++ dot_shellrc/media.sh
@@ -3,7 +3,7 @@
 
 ydl() {
 	cd "$HOME"/Downloads || exit 1
-	video_title=$(youtube-dl --skip-download --get-title --no-warnings "$@" | sed 2d)
+	video_title=$(yt-dlp --skip-download --get-title --no-warnings "$@" | sed 2d)
 	if yt-dlp -f 'bestvideo[height<=720]+bestaudio[ext=m4a]/best[height<=720]' --recode-video mp4 --audio-format m4a --merge-output-format mp4 "$@"; then
 		terminal-notifier -title "Video Download Finished" -sound default -message "$video_title"
 	else
@@ -13,7 +13,7 @@ ydl() {
 
 ymdl() {
 	cd "$HOME"/Downloads || exit 1
-	video_title=$(youtube-dl --skip-download --get-title --no-warnings "$@" | sed 2d)
+	video_title=$(yt-dlp --skip-download --get-title --no-warnings "$@" | sed 2d)
 	if yt-dlp -x --audio-format m4a --embed-thumbnail "$@"; then
 		terminal-notifier -title "Video Download Finished" -sound default -message "$video_title"
 	else
