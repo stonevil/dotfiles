16a721cb797d37b7e14b49f03c05b09e9ead2c62
Myroslav Rys <stonevil@gmail.com>
2022-09-08 10:15:45 -0700

    General fixing

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,36 +1,38 @@
 -- WezTerm
 -- https://wezfurlong.org/wezterm/
 
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-	window_close_confirmation = 'NeverPrompt',
-	exit_behavior = 'Close',
+	window_close_confirmation = "NeverPrompt",
+	exit_behavior = "Close",
 
-	audible_bell = 'Disabled',
+	audible_bell = "Disabled",
 
 	-- Smart tab bar [distraction-free mode]
 	hide_tab_bar_if_only_one_tab = true,
-	tab_bar_at_bottom = true,
+	tab_bar_at_bottom = false,
 
 	window_padding = {
-  	left = 3, right = 3,
-  	top = 3, bottom = 3,
-  },
+		left = 3,
+		right = 3,
+		top = 3,
+		bottom = 3,
+	},
 
 	-- Color scheme
 	-- https://wezfurlong.org/wezterm/colorschemes/index.html
-	color_scheme = 'Wez',
+	color_scheme = "Catppuccin Mocha",
 
 	window_background_opacity = 0.95,
 
 	-- Font configuration
 	-- https://wezfurlong.org/wezterm/config/fonts.html
-	font = wezterm.font('Iosevka Nerd Font'),
+	font = wezterm.font("Iosevka Nerd Font"),
 	font_size = 14.0,
 
 	-- Cursor style
-	default_cursor_style = 'BlinkingBlock',
+	default_cursor_style = "BlinkingBlock",
 
 	native_macos_fullscreen_mode = true,
 
