ddc57ccd91e8fec2f5ea85491138a7189ab1bf05
Myroslav Rys <stonevil@gmail.com>
2023-02-27 00:49:17 -0800

    Improve test procedure

--- .test.sh
+++ .test.sh
@@ -2,9 +2,22 @@
 # vim:ft=sh
 
 # Grab command name
-export DISTRO="${1:-alpine}"
-export DISTRO_VER="${2:-edge}"
+case "$DISTRO" in
+	ubuntu|debian) export DISTRO="debian"
+	;;
+	*) export DISTRO="${1:-alpine}"
+	;;
+esac
+
+case "$DISTRO" in
+	alpine) export DISTRO_VER="${2:-3.17}"
+	;;
+	fedora) export DISTRO_VER="${2:-37}"
+	;;
+	*) export DISTRO_VER="${2:-latest}"
+	;;
+esac
 
 export CLI="${3:-limactl shell docker docker}"
 
-limactl shell docker docker run -it --rm -v $PWD:/root/.local/share/chezmoi $DISTRO:$DISTRO_VER sh -c "/root/.local/share/chezmoi/.test/$DISTRO/run.sh"
+eval "$CLI" run -it --rm -v $PWD:/root/.local/share/chezmoi -e DISTRO=$DISTRO -e DISTRO_VER=$DISTRO_VER $DISTRO:$DISTRO_VER sh -c "/root/.local/share/chezmoi/.test/$DISTRO/run.sh"
--- .test/alpine/run.sh
+++ .test/alpine/run.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 # vim:ft=sh
 
-(echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories && \
-	echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories) || exit 1
+if [[ $DISTRO_VER == "edge" ]]; then
+	sed -i 's/v3.\b[0-9]\{2\}\b/edge/g' /etc/apk/repositories || exit 1
+else
+	sed -i "s/v3.\b[0-9]\{2\}\b/v$DISTRO_VER/g" /etc/apk/repositories || exit 1
+fi
 
-sed -i 's/v3.\b[0-9]\{2\}\b/edge/g' /etc/apk/repositories || exit 1
+(echo "https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories) || exit 1
 
 apk update && \
 	apk add sudo bash curl && \
