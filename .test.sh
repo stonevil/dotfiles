af935dca33524ce00cc49cce6fe840e92d1f3ac7
Myroslav Rys <stonevil@gmail.com>
2023-02-27 18:33:47 -0800

    A lot fixes for Ubuntu, Alpine. Overal improvement. Remove Ubuntu spyware.

--- .test.sh
+++ .test.sh
@@ -7,15 +7,11 @@ echo "Detect OS distro and version"
 case "$DISTRO" in
 	alpine) export DISTRO_VER="${2:-3.17}"
 	;;
-	archlinux) export DISTRO_VER="${2:-latest}"
+	archlinux|centos|ubuntu) export DISTRO_VER="${2:-latest}"
 	;;
 	fedora) export DISTRO_VER="${2:-37}"
 	;;
-	centos) export DISTRO_VER="${2:-latest}"
-	;;
-	ubuntu|debian) export DISTRO_DIR="debian"; export DISTRO_VER="${2:-latest}"
-	;;
-	*) export DISTRO="alpine"; DISTRO_DIR="alpine"; DISTRO_VER="3.17"
+	*) export DISTRO="alpine"; DISTRO_VER="3.17"
 	;;
 esac
 
@@ -23,4 +19,4 @@ echo "Last chance for Blade Runner"
 export CLI="${3:-limactl shell docker docker}"
 
 echo "Execute container"
-eval "$CLI" run -it --rm -v $PWD:/root/.local/share/chezmoi -e DISTRO=$DISTRO -e DISTRO_VER=$DISTRO_VER -e DISTRO_DIR:$DISTRO_DIR $DISTRO:$DISTRO_VER sh -c "/root/.local/share/chezmoi/.test/$DISTRO_DIR/run.sh"
+eval "$CLI" run -it --rm -v $PWD:/root/.local/share/chezmoi -e DISTRO=$DISTRO -e DISTRO_VER=$DISTRO_VER $DISTRO:$DISTRO_VER sh -c "/root/.local/share/chezmoi/.test/$DISTRO/run.sh"
