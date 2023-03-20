6fd7f063c3ee82495c21954e267c5c5c980ecfa1
Myroslav Rys <stonevil@gmail.com>
2023-03-20 00:08:40 -0700

    Add help for keymaps

--- private_dot_config/exact_nvim/exact_lua/config/keymaps.lua
+++ private_dot_config/exact_nvim/exact_lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Uncomment for keybinding
+-- local keymap = vim.keymap.set
