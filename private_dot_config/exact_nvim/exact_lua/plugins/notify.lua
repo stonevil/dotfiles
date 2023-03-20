ae429506a1115ca6d5490e7d2f7b9ac803053de3
Myroslav Rys <stonevil@gmail.com>
2023-03-19 20:50:35 -0700

    Update size and timeout

--- private_dot_config/exact_nvim/exact_lua/plugins/notify.lua
+++ private_dot_config/exact_nvim/exact_lua/plugins/notify.lua
@@ -5,8 +5,14 @@ return {
 		config = function()
 			require("notify").setup({
 				background_colour = "#000000",
-				timeout = 1000,
+				timeout = 2000,
 				fps = 60,
+				max_height = function()
+					return math.floor(vim.o.lines * 0.75)
+				end,
+				max_width = function()
+					return math.floor(vim.o.columns * 0.75)
+				end,
 			})
 		end,
 	},
