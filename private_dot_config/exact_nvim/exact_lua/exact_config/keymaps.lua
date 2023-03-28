27965b405a859af6f9f4294e865cbd1db2d22a6f
Myroslav Rys <stonevil@gmail.com>
2023-03-28 05:36:05 -0700

    Remove nvim config

--- private_dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Uncomment for keybinding
--- local keymap = vim.keymap.set
--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++
@@ -1,5 +0,0 @@
--- Override the configuration of LazyVim plugins
-return {
-	{ "nvim-lua/plenary.nvim" },
-	{ "nacro90/numb.nvim", event = "BufReadPre", config = true },
-}
