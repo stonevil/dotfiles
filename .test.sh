a9fcfd55f64b6fe5f3bb31f1ab229c3810036ee4
Myroslav Rys <stonevil@gmail.com>
2023-03-29 12:11:02 -0700

    Switch to podman. Improve detection.

--- .test.sh
+++ .test.sh
@@ -1,9 +1,39 @@
 #!/usr/bin/env bash
 # vim:ft=sh
 
+_help() {
+	# Display Help
+	echo "Command syntax:"
+	echo -e "./.test.sh [distro] [version]\n"
+	echo "Example with Alpine Edge Linux:"
+	echo -e "./.test.sh alpine edge\n"
+	echo -e "Supported distro: Fedora Linux v37 and newer, Alpine Linux Edge\n"
+}
+
+_check() {
+	# Check requirements
+	if command -v limactl >/dev/null 2>&1; then
+		export CLI="limactl shell podman podman"
+	else
+		if command -v podman >/dev/null 2>&1; then
+			export CLI="podman"
+		else
+			echo -e "At least limactl or podman should be installed for testing container environments\n"
+			exit 1
+		fi
+	fi
+	# Detect if limactl podman is running
+	if [[ $OSTYPE == 'darwin'* ]]; then
+		if [[ $(limactl list podman --format "{{.Status}}") == "Stopped" ]]; then
+			echo -e "limactl podman instance is not running\n"
+			exit 1
+		fi
+	fi
+}
+
 export DISTRO=$1
 
-echo "Detect OS distro and version"
+# Detect OS distro and version
 case "$DISTRO" in
 alpine)
 	export IMAGE="$DISTRO"
@@ -21,6 +51,10 @@ fedora)
 	export IMAGE="$DISTRO"
 	export IMAGE_TAG="${2:-37}"
 	;;
+help | '--help' | '-h')
+	_help
+	exit 0
+	;;
 *)
 	export DISTRO="alpine"
 	IMAGE="$DISTRO"
@@ -28,8 +62,9 @@ fedora)
 	;;
 esac
 
-echo "Last chance for Blade Runner"
-export CLI="${3:-limactl shell docker docker}"
+# Check requirements
+_check
 
+# Execute testing environment
 echo "Execute container"
 ${CLI} run -it --rm -v "$PWD":/root/.local/share/chezmoi -e DISTRO="$DISTRO" -e IMAGE_TAG="$IMAGE_TAG" "$IMAGE":"$IMAGE_TAG" sh -c "/root/.local/share/chezmoi/.entrypoint.sh"
