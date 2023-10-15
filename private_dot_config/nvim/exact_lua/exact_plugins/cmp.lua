e47299d0a74fa94d5789d0646c6a5ae2de81d881
Myroslav Rys <stonevil@gmail.com>
2023-10-14 23:10:41 -0700

    Simplify cmp-emoji

--- private_dot_config/nvim/exact_lua/exact_plugins/cmp.lua
+++ private_dot_config/nvim/exact_lua/exact_plugins/cmp.lua
@@ -55,8 +55,7 @@ return {
 		dependencies = { "hrsh7th/cmp-emoji" },
 		---@param opts cmp.ConfigSchema
 		opts = function(_, opts)
-			local cmp = require("cmp")
-			opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+			table.insert(opts.sources, { name = "emoji" })
 		end,
 	},
 
@@ -71,7 +70,8 @@ return {
 			local has_words_before = function()
 				unpack = unpack or table.unpack
 				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
 
 			local luasnip = require("luasnip")
