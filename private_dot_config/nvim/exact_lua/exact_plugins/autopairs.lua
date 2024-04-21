9cb401165a639fb232e7f1496f0a98287e41f043
Myroslav Rys <stonevil@gmail.com>
2024-04-21 01:10:02 -0700

    NVIM: add autopairs plugin

---
+++ private_dot_config/nvim/exact_lua/exact_plugins/autopairs.lua
@@ -0,0 +1,18 @@
+return {
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	opts = {
+		check_ts = true,
+	},
+	config = function(_, opts)
+		require("nvim-autopairs").setup(opts)
+
+		--- setup for cmp
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		local cmp_status_ok, cmp = pcall(require, "cmp")
+		if not cmp_status_ok then
+			return
+		end
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
+}
