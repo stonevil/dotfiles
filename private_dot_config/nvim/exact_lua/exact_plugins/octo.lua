bd919864b0fab0b27735ec9ddb2b189d8ca54790
Myroslav Rys <stonevil@gmail.com>
2024-04-27 19:23:12 -0700

    NVIM: fix octo plugin suppress missing scope

--- private_dot_config/nvim/exact_lua/exact_plugins/octo.lua
+++ private_dot_config/nvim/exact_lua/exact_plugins/octo.lua
@@ -3,10 +3,13 @@ return {
 	requires = {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope.nvim",
-		-- OR 'ibhagwan/fzf-lua',
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("octo").setup()
+		require("octo").setup({
+			suppress_missing_scope = {
+				projects_v2 = true,
+			},
+		})
 	end,
 }
