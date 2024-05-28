03063a958dad00e31d27153bc7a93d180f856693
Myroslav Rys <stonevil@gmail.com>
2024-05-27 21:22:01 -0700

    NVIM: add dap

---
+++ private_dot_config/nvim/exact_lua/exact_plugins/dap.lua
@@ -0,0 +1,82 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			local dap = require("dap")
+			local ui = require("dapui")
+
+			require("dapui").setup()
+			require("dap-go").setup()
+
+			require("nvim-dap-virtual-text").setup({
+				display_callback = function(variable)
+					local name = string.lower(variable.name)
+					local value = string.lower(variable.value)
+					if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
+						return "*****"
+					end
+
+					if #variable.value > 15 then
+						return " " .. string.sub(variable.value, 1, 15) .. "... "
+					end
+
+					return " " .. variable.value
+				end,
+			})
+
+			local elixir_ls_debugger = vim.fn.exepath("elixir-ls-debugger")
+			if elixir_ls_debugger ~= "" then
+				dap.adapters.mix_task = {
+					type = "executable",
+					command = elixir_ls_debugger,
+				}
+
+				dap.configurations.elixir = {
+					{
+						type = "mix_task",
+						name = "phoenix server",
+						task = "phx.server",
+						request = "launch",
+						projectDir = "${workspaceFolder}",
+						exitAfterTaskReturns = false,
+						debugAutoInterpretAllModules = false,
+					},
+				}
+			end
+
+			vim.keymap.set("n", "<F8>", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<F6>", dap.run_to_cursor)
+
+			vim.keymap.set("n", "<F7>", function()
+				require("dapui").eval(nil, { enter = true })
+			end)
+
+			vim.keymap.set("n", "<F1>", dap.continue)
+			vim.keymap.set("n", "<F2>", dap.step_into)
+			vim.keymap.set("n", "<F3>", dap.step_over)
+			vim.keymap.set("n", "<F4>", dap.step_out)
+			vim.keymap.set("n", "<F5>", dap.step_back)
+			vim.keymap.set("n", "<F12>", dap.restart)
+
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
+			end
+		end,
+	},
+}
