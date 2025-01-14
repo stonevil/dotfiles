4028276751270c98c712bf6b68b87839b85f67d5
Myroslav Rys <stonevil@gmail.com>
2025-01-13 21:35:44 -0800

    YAZI: fix various issues

--- private_dot_config/nvim/exact_lua/exact_plugins/yazi.lua
+++ private_dot_config/nvim/exact_lua/exact_plugins/yazi.lua
@@ -4,20 +4,20 @@ return {
 	keys = {
 		-- 👇 in this section, choose your own keymappings!
 		{
-			"<leader>Y",
+			"<leader>YY",
 			"<cmd>Yazi<cr>",
 			desc = "Open yazi at the current file",
 		},
 		{
 			-- Open in the current working directory
-			"<leader>YC",
+			"<leader>YH",
 			"<cmd>Yazi cwd<cr>",
 			desc = "Open the file manager in nvim's working directory",
 		},
 		{
 			-- NOTE: this requires a version of yazi that includes
 			-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-			"<leader>YY",
+			"<leader>YT",
 			"<cmd>Yazi toggle<cr>",
 			desc = "Resume the last yazi session",
 		},
