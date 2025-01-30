ccb0ed9d979699b37e83c92b82e266a92eb86682
Myroslav Rys <stonevil@gmail.com>
2025-01-29 20:18:10 -0800

    NVIM: switch to smart-splits

--- private_dot_config/nvim/exact_lua/exact_plugins/navigator.lua
+++ private_dot_config/nvim/exact_lua/exact_plugins/navigator.lua
@@ -1,36 +1,30 @@
 return {
 	{
-		"numToStr/Navigator.nvim",
+		"mrjones2014/smart-splits.nvim",
 		config = function()
-			require("Navigator").setup()
-			vim.keymap.set({ "n", "t" }, "<A-Left>", "<CMD>NavigatorLeft<CR>")
-			vim.keymap.set({ "n", "t" }, "<A-Right>", "<CMD>NavigatorRight<CR>")
-			vim.keymap.set({ "n", "t" }, "<A-Up>", "<CMD>NavigatorUp<CR>")
-			vim.keymap.set({ "n", "t" }, "<A-Down>", "<CMD>NavigatorDown<CR>")
-			vim.keymap.set({ "n", "t" }, "<A-p>", "<CMD>NavigatorPrevious<CR>")
+			require("smart-splits").setup({
+				default_amount = 3,
+				at_edge = "wrap",
+				log_level = "info",
+			})
+			-- these keymaps will also accept a range,
+			-- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+			vim.keymap.set("n", "<A-S-Left>", require("smart-splits").resize_left)
+			vim.keymap.set("n", "<A-S-Down>", require("smart-splits").resize_down)
+			vim.keymap.set("n", "<A-S-Up>", require("smart-splits").resize_up)
+			vim.keymap.set("n", "<A-S-Right>", require("smart-splits").resize_right)
+			-- moving between splits
+			vim.keymap.set("n", "<A-Left>", require("smart-splits").move_cursor_left)
+			vim.keymap.set("n", "<A-Down>", require("smart-splits").move_cursor_down)
+			vim.keymap.set("n", "<A-Up>", require("smart-splits").move_cursor_up)
+			vim.keymap.set("n", "<A-Right>", require("smart-splits").move_cursor_right)
+			-- TODO: sync with tmux
+			-- vim.keymap.set('n', '<C-\\>', require('smart-splits').move_cursor_previous)
+			-- swapping buffers between windows
+			vim.keymap.set("n", "<leader><leader>Left", require("smart-splits").swap_buf_left)
+			vim.keymap.set("n", "<leader><leader>Down", require("smart-splits").swap_buf_down)
+			vim.keymap.set("n", "<leader><leader>Up", require("smart-splits").swap_buf_up)
+			vim.keymap.set("n", "<leader><leader>Right", require("smart-splits").swap_buf_right)
 		end,
 	},
-	-- {
-	-- 	"aserowy/tmux.nvim",
-	-- 	config = function()
-	-- 		require("tmux").setup({
-	-- 			navigation = {
-	-- 				enable_default_keybindings = false,
-	-- 			},
-	-- 			resize = {
-	-- 				enable_default_keybindings = false,
-	-- 				resize_step_x = 4,
-	-- 				resize_step_y = 4,
-	-- 			},
-	-- 			vim.keymap.set({ "n", "t" }, "<A-Left>", '<cmd>lua require("tmux").move_left()<cr>'),
-	-- 			vim.keymap.set({ "n", "t" }, "<A-Right>", '<cmd>lua require("tmux").move_right()<cr>'),
-	-- 			vim.keymap.set({ "n", "t" }, "<A-Up>", '<cmd>lua require("tmux").move_up()<cr>'),
-	-- 			vim.keymap.set({ "n", "t" }, "<A-Down>", '<cmd>lua require("tmux").move_down()<cr>'),
-	-- 			vim.keymap.set({ "n", "t" }, "<A-S-Left>", '<cmd>lua require("tmux").resize_left()<cr>'),
-	-- 			vim.keymap.set({ "n", "t" }, "<A-S-Right>", '<cmd>lua require("tmux").resize_right()<cr>'),
-	-- 			vim.keymap.set({ "n", "t" }, "<A-S-Up>", '<cmd>lua require("tmux").resize_up()<cr>'),
-	-- 			vim.keymap.set({ "n", "t" }, "<A-S-Down>", '<cmd>lua require("tmux").resize_down()<cr>'),
-	-- 		})
-	-- 	end,
-	-- },
 }
