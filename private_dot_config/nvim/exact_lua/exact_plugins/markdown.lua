ae3811f28da9bef15c8f3d148c01332f72043bc8
Myroslav Rys <stonevil@gmail.com>
2025-05-05 23:03:47 -0700

    NVIM: fix issues with markdown build command

--- private_dot_config/nvim/exact_lua/exact_plugins/markdown.lua
+++ private_dot_config/nvim/exact_lua/exact_plugins/markdown.lua
@@ -10,8 +10,6 @@ return {
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
+		build = ":call mkdp#util#install()",
 	},
 }
