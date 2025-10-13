5c35fd9c5b58633255741b925d12e05e517548d2
Myroslav Rys <stonevil@gmail.com>
2025-10-12 18:38:06 -0700

    NVIM: improve snacks config

---
+++ private_dot_config/nvim/exact_lua/exact_plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				sources = {
+					explorer = {
+						focus = "input",
+						auto_close = true,
+					},
+				},
+			},
+		},
+	},
+}
