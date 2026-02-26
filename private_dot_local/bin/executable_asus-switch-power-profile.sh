6e4c40c6c17bfa60bcbc121fbb30f339b273509c
Myroslav Rys <stonevil@gmail.com>
2026-02-26 05:35:13 -0800

    ASUS: update script to switch profiles

--- private_dot_local/bin/executable_asus-switch-power-profile.sh
+++ private_dot_local/bin/executable_asus-switch-power-profile.sh
@@ -5,4 +5,4 @@ set -eu # Fail on errors or undeclared variables
 
 profile=$(kdialog --radiolist "Select Power Profile:" Performance "Performance" on Balanced "Balanced" off Quiet "Quiet" off)
 
-asusctl profile -P $profile && notify-send --hint=string:desktop-entry:rog-control-center $profile
+asusctl profile set $profile && notify-send --hint=string:desktop-entry:rog-control-center "  Power profile set to: $profile"
