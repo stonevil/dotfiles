7a0f2ded4c59cec61ff926ac9fddf7dd10514628
Myroslav Rys <stonevil@gmail.com>
2026-09-24 22:24:17 -0700

    CHEZMOI: fix test script

--- .test.sh
+++ .test.sh
@@ -7,31 +7,25 @@ arch=$2
 # Display Help
 _help() {
 	echo "Command syntax:"
-	echo -e "./.test.sh [distro] [arch]\n"
+	echo -e "./.test.sh [distro]\n"
 	echo "Example with Fedora Linux:"
-	echo -e "./.test.sh fedora amd64\n"
+	echo -e "./.test.sh fedora\n"
 	echo -e "Supported distro: arch, fedora\n"
-	echo -e "Supported architecture: amd64, arm64\n"
 }
 
 # Detect OS distro and architecture
 case $distro in
 arch)
+	lima_instance="arch"
 	;;
 fedora)
+	lima_instance="arm64"
 	;;
-help | '--help' | '-h')
+help | '--help' | '-h' | *)
 	_help
 	exit 0
 	;;
-*)
-	echo -e "Unsupported distro\n Archlinux and AMD64 will be used"
-	distro="arch"
-	arch="amd64"
-	;;
 esac
-
-lima_instance="$distro-$arch"
 
 if [[ $(limactl list $lima_instance --format "{{.Status}}") == "Running" ]]; then
 	read -p "Install $lima_instance is running. Do you want to stop and reset instance? (Y/n): " -n 1 -r REPLY
